5597eef7119755bdf326c1b0262839ea35908908
MATHEUS MENDES <matheus.mendes@somosicev.com>
2024-03-24 15:57:23 -0300

    alterações

--- Jogo secreto/app.js
+++ Jogo secreto/app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
      exibirTextoNaTela('h1', 'Seja bem-vindo ao jogo do Mendes');
-     exibirTextoNaTela('p', 'Digite um na 10: ');
+     exibirTextoNaTela('p', 'Digite um número de 1 à 10: ');
 }
 exibirMensagemInicial();
 function verificarChute() {
